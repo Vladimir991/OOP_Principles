ab5bf1a30e70e109643c891bed2762c56985a2a1
Vladimir Voskoboev <wiz991@mail.ru>
2023-03-08 12:44:10 +0300

    initial commit

---
+++ src/main/java/ru/netology/oop/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.oop;
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле для паспорта
+    public String patronymic; //поле для отчества
+    public String phone; //поле для телефона
+    public String surname; //поле для фамилии
+    public boolean subcription;
+    public FormDate birthday;
+
+}
